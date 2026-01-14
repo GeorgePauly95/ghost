f294cf15cbae256b64a2d73011038eb0e4a316e7
GeorgePauly95 <georgepauly1995@gmail.com>
2026-01-15 04:06:12 +0530

    include episode metadata in text to be embedded

--- chunking.py
+++ chunking.py
@@ -17,15 +17,31 @@ def _get_episode_metadata(episode):
     return episode_metadata
 
 
+def _stringify_episode_metadata(episode_metadata):
+    return (
+        f"This is episode {episode_metadata['episode_number']}, titled {episode_metadata['title']}. "
+        f"It was published on {episode_metadata['date']}. The link to the episode is {episode_metadata['link']}"
+    )
+
+
 def _create_episode_chunks(episode):
     episode_metadata = _get_episode_metadata(episode)
     show_notes = episode.get("show notes")
+    metadata_string = _stringify_episode_metadata(episode_metadata)
     if show_notes is None:
-        return [{**episode_metadata, "show_notes_chunk": ""}]
+        return [
+            {
+                **episode_metadata,
+                "text_to_embed": f"{metadata_string}",
+            }
+        ]
     show_notes_chunks = splitter.split_text(show_notes)
     episode_chunks = []
     for show_notes_chunk in show_notes_chunks:
-        episode_chunk = {**episode_metadata, "show_notes_chunk": show_notes_chunk}
+        episode_chunk = {
+            **episode_metadata,
+            "text_to_embed": f"{metadata_string}\n\n{show_notes_chunk}",
+        }
         episode_chunks.append(episode_chunk)
     return episode_chunks
 
