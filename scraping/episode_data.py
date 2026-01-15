b7a4cec2301d9a712801015f76dc6a5d5ebe40a9
GeorgePauly95 <georgepauly1995@gmail.com>
2026-01-15 17:50:00 +0530

    use datetime info available on the website

--- scraping/episode_data.py
+++ scraping/episode_data.py
@@ -14,19 +14,22 @@ options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
 
 episodes_data = []
-
+counter = 0
 for url in episode_urls:
     driver.implicitly_wait(5)
 
     driver.get(url)
 
     title_element = driver.find_element(By.TAG_NAME, "h1")
-    time_element = driver.find_element(By.TAG_NAME, "time")
+    print(url)
+    time_element = driver.find_element(By.CSS_SELECTOR, ".entry-date.published")
     show_notes_element = driver.find_element(By.CLASS_NAME, "entry-content")
     episode_data = f"""'title': {title_element.text}\n
-        'date': {time_element.text}\n
+        'date': {time_element.get_attribute("datetime")}\n
         'show_notes': {show_notes_element.text}\n
         """
+    counter += 1
+    print(f"Percentage Done: {(counter * 100) / len(episode_urls)}")
     episodes_data.append(episode_data)
 
 
