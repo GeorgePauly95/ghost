3730bc6dae0e18be05e360126cd63045fc2cb63b
GeorgePauly95 <georgepauly1995@gmail.com>
2026-01-29 16:08:00 +0530

    separate validating from scraping

---
+++ validating.py
@@ -0,0 +1,21 @@
+def validate_episodes(db, episodes):
+    stored_episodes = db.get_all_episodes()
+    stored_episodes = {
+        stored_episode["link"]: stored_episode["sitemap_date"]
+        for stored_episode in stored_episodes
+    }
+    urls = [episode["url"] for episode in episodes]
+    new_urls = [url for url in urls if url not in stored_episodes]
+    modified_urls = []
+    for episode in episodes:
+        if episode["url"] in episodes:
+            if episode["sitemap_date"] > stored_episodes[episode["url"]]:
+                modified_urls.append(episode["url"])
+    return {"new_urls": new_urls, "modified_urls": modified_urls}
+
+
+def update_stored_episodes(db, validated_episodes):
+    modified_episodes = validated_episodes["modified_urls"]
+    db.delete_episodes(modified_episodes)
+    episodes_to_crawl = modified_episodes + validated_episodes["new_urls"]
+    return
