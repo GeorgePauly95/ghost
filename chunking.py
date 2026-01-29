351acf4158ee23ec579e7fbfdb5057e15d985001
GeorgePauly95 <georgepauly1995@gmail.com>
2026-01-29 16:02:18 +0530

    Remove dependency on storing data in a csv file. Corrected key 'show notes' to 'show_notes' Use regex to get 'episode number' and 'episode title' Use float to represent Episode Numbers since there is a Episode 40.5

--- chunking.py
+++ chunking.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from utils import extract_episode_number, extract_episode_title
 
 splitter = RecursiveCharacterTextSplitter(
     chunk_size=1200, chunk_overlap=200, length_function=len, is_separator_regex=False
@@ -8,11 +8,14 @@ splitter = RecursiveCharacterTextSplitter(
 
 
 def _get_episode_metadata(episode):
+    episode_number_and_title = episode.get("title")
+    title = extract_episode_title(episode_number_and_title)
+    episode_number = extract_episode_number(episode_number_and_title)
     episode_metadata = {
-        "title": episode.get("title"),
+        "title": title,
         "date": episode.get("date"),
         "link": episode.get("link"),
-        "episode_number": int(episode.get("episode number")),
+        "episode_number": float(episode_number),
     }
     return episode_metadata
 
@@ -26,7 +29,7 @@ def _stringify_episode_metadata(episode_metadata):
 
 def _create_episode_chunks(episode):
     episode_metadata = _get_episode_metadata(episode)
-    show_notes = episode.get("show notes")
+    show_notes = episode.get("show_notes")
     metadata_string = _stringify_episode_metadata(episode_metadata)
     if show_notes is None:
         return [
@@ -46,14 +49,7 @@ def _create_episode_chunks(episode):
     return episode_chunks
 
 
-def _ingest_file(file_name):
-    df = pd.read_csv(f"{file_name}.csv")
-    return df
-
-
-def create_podcast_chunks(file_name):
-    df = _ingest_file(file_name)
-    episodes = [row.to_dict() for _, row in df.iterrows()]
+def create_podcast_chunks(episodes):
     podcast_chunks = []
     for episode in episodes:
         podcast_chunks += _create_episode_chunks(episode)
