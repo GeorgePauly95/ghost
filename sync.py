bc620e870cb8d09242ad97edffce98e5be0614a2
GeorgePauly95 <georgepauly1995@gmail.com>
2026-01-30 11:33:10 +0530

    separate out main sync function

--- sync.py
+++ sync.py
@@ -1,8 +1,8 @@
 from scraping import Scraper
 from validating import validate_episodes
-from postgres import Postgres
 from chunking import create_podcast_chunks
 from embedding import create_embedding
+from postgres import Postgres
 
 
 def sync():
@@ -10,19 +10,31 @@ def sync():
     pg = Postgres()
     episodes = scraper.get_episodes()
     validated_episodes = validate_episodes(pg, episodes)
-    modified_episodes = validated_episodes["modified_urls"]
-    pg.delete_episodes(modified_episodes)
-    urls_to_be_scraped = modified_episodes + validated_episodes["new_urls"]
+    update_episodes(pg, scraper, validated_episodes)
+
+
+def update_episodes(db, scraper, validated_episodes):
+    modified_urls = validated_episodes["modified_urls"]
+    new_urls = validated_episodes["new_urls"]
+    if modified_urls == [] and new_urls == []:
+        print(
+            "No new episodes have been released and past episodes haven't been updated"
+        )
+    elif modified_urls != [] and new_urls == []:
+        db.delete_episodes(modified_urls)
+        print(f"The following episodes have been modified: {modified_urls}")
+    elif modified_urls == [] and new_urls != []:
+        print(f"The following episodes have been released: {new_urls}")
+    else:
+        print(f"""The following episodes have been modified: {modified_urls}\n\n
+                The following episodes have been released: {new_urls}""")
+    urls_to_be_scraped = modified_urls + new_urls
     episodes = scraper.get_episode_details(urls_to_be_scraped)
-    print(f"Episodes are: {episodes}")
     chunks = create_podcast_chunks(episodes)
     chunk_count = len(chunks)
     counter = 0
     for chunk in chunks:
         embedded_chunk = create_embedding(chunk)
-        pg.add_episode(embedded_chunk)
+        db.add_episode(embedded_chunk)
         counter += 1
         print(f"Percentage Embedding and Storing done: {(counter * 100) / chunk_count}")
-
-
-sync()
